04d119963b4ce187533dc77060dc9c9d3f270f27
Suskz <ielsondias1@gmail.com>
2023-09-19 11:32:23 -0300

    first commit

---
+++ myApp.js
@@ -0,0 +1,41 @@
+let express = require('express');
+let app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
