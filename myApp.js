37faf788a40dd2cd2edbf7c5310fb1db4c25ce69
Suskz <ielsondias1@gmail.com>
2023-09-20 19:20:03 -0300

    chain-middleware-to-create-a-time-server

--- myApp.js
+++ myApp.js
@@ -1,29 +1,39 @@
 let express = require('express');
 let app = express();
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
-});
+/*app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});*/
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 })
 
-app.use("/public", express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-    if (process.env["MESSAGE_STYLE"] === "uppercase") {
-      res.json({
-        message: "Hello json".toUpperCase()
-      })
-    } else {
-      res.json({
-        message: "Hello json"
-      })
-    }
+  if (process.env["MESSAGE_STYLE"] === "uppercase") {
+    res.json({
+      message: "Hello json".toUpperCase()
+    })
+  } else {
+    res.json({
+      message: "Hello json"
+    })
+  }
 })
 
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+},
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
 
 
 
@@ -51,4 +61,10 @@ app.get("/json", (req, res) => {
 
 
 
- module.exports = app;
+
+
+
+
+
+
+module.exports = app;
